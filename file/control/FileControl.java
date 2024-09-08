14bd50f222cfe5c2f1941aa30b1859e15905eec1
Lee YeEun <78542145+yerinfan@users.noreply.github.com>
2024-09-08 15:01:28 +0900

    240908 수정
    파일 생성, 디렉토리 생성 기능 추가 및 경로 이동 오류 수정

--- file/control/FileControl.java
+++ file/control/FileControl.java
@@ -7,26 +7,34 @@ import kr.ac.kopo.file.view.consoleView;
 
 public class FileControl {
 
+	private String currentPath = "C:/";
+	
 	public void start() throws IOException {
 		Scanner sc = new Scanner(System.in);
 		consoleView view = new consoleView();
 		String s;
 
 		while (true) {
-			view.viewRoute();
+			view.viewRoute(currentPath);
 			s = sc.nextLine();
-
+			
 			switch (s) {
 			case ("q"):
 				return;
 			case ("m"):
-				view.move();
+				currentPath += view.move();
 				break;
 			case ("rm"):
 				view.remove();
 				break;
 			case ("r"):
 				view.rename();
+				break;
+			case ("n") :
+				view.newFile();
+				break;
+			case ("mkdir") :
+				view.mkdir();
 				break;
 			}
 
--- file/view/consoleView.java
+++ file/view/consoleView.java
@@ -1,6 +1,7 @@
 package kr.ac.kopo.file.view;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class consoleView {
@@ -12,9 +13,9 @@ public class consoleView {
 		sc = new Scanner(System.in);
 	}
 	
-	public void viewRoute() {
+	public void viewRoute(String currentPath) {
 
-		File dir = new File("C:/"); 
+		File dir = new File(currentPath); 
 		File[] fileList = dir.listFiles();
 
 		if (fileList != null) {
@@ -36,12 +37,12 @@ public class consoleView {
 
 		}
 
-		System.out.println("\n폴더이동(m), 이름변경(r), 파일삭제(rm), 종료(q): ");
+		System.out.println("\n폴더이동(m), 이름변경(r), 파일 생성(n), 디렉토리 생성(mkdir), 파일삭제(rm), 종료(q): ");
 		
 		
 	}
 
-	public void move() {
+	public String move() {
 		
 		System.out.println("이동할 폴더 이름을 입력하세요: ");
 		String folderName = sc.nextLine();
@@ -51,6 +52,8 @@ public class consoleView {
 		} else {
 			System.out.println("존재하지 않는 폴더입니다.");
 		}
+		
+		return folderName;
 	}
 
 	public void remove() {
@@ -60,7 +63,7 @@ public class consoleView {
 		File fileToDelete = new File(currentPath + "/" + fileName);
 		if (fileToDelete.isFile()) {
 			if (fileToDelete.delete()) {
-				System.out.println(fileName + "이(가) 삭제되었습니다.");
+				System.out.println(fileName + " 삭제 완료");
 			} else {
 				System.out.println("파일 삭제에 실패했습니다.");
 			}
@@ -79,11 +82,40 @@ public class consoleView {
 		File newFile = new File(currentPath + "/" + newName);
 
 		if (oldFile.renameTo(newFile)) {
-			System.out.println(oldName + "이(가) " + newName + "으로 변경되었습니다.");
+			System.out.println(oldName + "이(가) " + newName + "으로 변경됨.");
 		} else {
-			System.out.println("이름 변경에 실패했습니다.");
+			System.out.println("이름 변경에 실패하였습니다.");
 		}
 	}
+
+	public void newFile() {
+		System.out.println("생성 할 파일의 이름을 설정하세요. : ");
+		String fileName = sc.nextLine();
+		
+		 File newFile = new File(currentPath, fileName);
+	        try {
+	            if (newFile.createNewFile()) {
+	                System.out.println("파일 생성 : " + newFile.getName());
+	            } else {
+	                System.out.println("파일이 이미 존재합니다.");
+	            }
+	        } catch (IOException e) {
+	            System.out.println("파일 생성 중 오류 발생.");
+	            e.printStackTrace();
+	        }
+		
+	}
 	
+	public void mkdir() {
+		System.out.print("생성 할 디렉토리명을 설정하세요. :");
+		String dirName = sc.nextLine();
+		
+        File newDir = new File(currentPath, dirName);
+        if (newDir.mkdir()) {
+            System.out.println("생성된 디렉토리 : " + newDir.getName());
+        } else {
+            System.out.println("이미 존재하거나 생성할 수 없습니다.");
+        }
+    }
 
 }
